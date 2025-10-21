bf4948d5a52a4b62a1183ec35e68cb2921004721
Doknot Byte <doknotbyte@gmail.com>
2025-10-21 21:55:17 +0800

    creation of act 3

--- lab11/fill-blanks.js
+++ lab11/fill-blanks.js
@@ -1,6 +1,6 @@
 // Activity 3: Fill-in-the-Blanks Code
 
-let name = "Jade";
-let age = 20;
+let name = "Jade Louisse R. Benguit";
+let age = 19;
 
 console.log("Hi, my name is " + name + " and I am " + age + " years old.");
